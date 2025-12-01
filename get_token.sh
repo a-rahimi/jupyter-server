eb8be3c0615641af5aa90e30df824e87cb53aca6
Ali Rahimi <ali@leavemeout.com>
2025-12-01 08:09:00 -0800

    a simpler way to get the token

--- get_token.sh
+++ get_token.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 while true; do
-    TOKEN=$(DOCKER_HOST="ssh://ali@server.local" docker logs jupyter-server 2>&1 | \
-        grep -o 'http://127.0.0.1:8888/lab?token=[a-zA-Z0-9]*' | \
-        tail -n 1 | \
-        sed 's/.*token=//')
+    TOKEN=$(DOCKER_HOST="ssh://ali@server.local" docker exec jupyter-server jupyter server list 2>&1 | \
+        sed -nE '/token=/ { s/.*token=([a-z0-9]+).*/\1/p; q; }')
 
     if [ -n "$TOKEN" ]; then
         echo "$TOKEN"
